ee03ae29ac513db96941e4f54974a15502539288
Amélie <ada125@student.bham.ac.uk>
2023-12-19 13:06:34 +0000

    Add destructable blocks with varying strengths.

--- Assets/Destructable.cs
+++ Assets/Destructable.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxDamage = 100f; // Maximum damage the block can take before being destroyed
+    private float currentDamage = 0f; // Current damage level of the block
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        // Calculate damage based on collision force
+        float damage = collision.relativeVelocity.magnitude;
+
+        // Add an additional damage condition for falling from height
+        if (collision.gameObject.CompareTag("Ground") && transform.position.y >= 3 * Mathf.Max(transform.localScale.x, transform.localScale.y))
+        {
+            damage *= 2; // Increase damage if falling from a height
+        }
+
+        // Apply damage if colliding with a projectile
+        if (collision.gameObject.CompareTag("Projectile"))
+        {
+            TakeDamage(damage);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void TakeDamage(float damage)
     {
-        
+        currentDamage += damage;
+        if (currentDamage >= maxDamage)
+        {
+            Destroy(gameObject); // Destroy the block if it exceeds the max damage
+        }
     }
 }
