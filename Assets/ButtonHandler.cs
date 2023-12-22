9253d025ec218ceb6467533f8408585b9f7e7a31
Amélie <ada125@student.bham.ac.uk>
2023-12-22 23:55:12 +0000

    Add buttonhandler

---
+++ Assets/ButtonHandler.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class DetectClicks : MonoBehaviour
+{
+    public string tagToHide = "Scene1";
+
+    private void OnMouseDown()
+    {
+        // Find the GameObject with the specified tag
+        GameObject objectToHide = GameObject.FindGameObjectWithTag(tagToHide);
+
+        // If the object is found, deactivate it
+        if (objectToHide != null)
+        {
+            objectToHide.SetActive(false);
+            UnityEngine.Debug.Log("Clicked and hid the object with tag: " + tagToHide);
+        }
+        else
+        {
+            UnityEngine.Debug.Log("No object with the tag " + tagToHide + " found!");
+        }
+    }
+}
